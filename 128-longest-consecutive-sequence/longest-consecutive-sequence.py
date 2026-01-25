817245234b662ec4b084f1ff185970defee5bc1d
Anh Luu <101529755+anhluu14@users.noreply.github.com>
2026-01-24 23:48:54 -0500

    Time: 55 ms (38.39%) | Memory: 36.6 MB (31.78%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s = set(nums) #một kiểu dữ liệu trong Python dùng để lưu tập hợp các phần tử KHÔNG TRÙNG LẶP, và quan trọng nhất là kiểm tra tồn tại rất nhanh (O(1)).
+        longest = 0
+        for x in s:
+            #check if its the start of sequence
+            if x - 1 not in s: #if it does not have a left neighbor then its the start of the sequence 
+                length = 0
+                while x + length in s:
+                    length += 1
+                longest = max(longest, length)
+        return longest
