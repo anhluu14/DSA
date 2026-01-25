d03ccd32fbd7aee60e08c50b3c9c060ce230b171
Anh Luu <101529755+anhluu14@users.noreply.github.com>
2026-01-25 00:18:05 -0500

    Time: 3 ms (70.51%) | Memory: 19.4 MB (36.47%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,15 +1,6 @@
 class Solution:
-    def f(self, i: int) -> int:
-        if i == 0:
-            return 0
-        if i == 1:
-            return 0
-        return min(self.f(i - 1) + self.nums[i - 1], self.f(i - 2) + self.nums[i - 2])
-        
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
-        self.cost = cost.copy()
-
         dp = [0] * (n + 1)
         dp[0] = 0
         dp[1] = 0
