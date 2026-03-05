13089fb9cb2a1212aaa07c2480f5314e1d5b936b
Anh Luu <101529755+anhluu14@users.noreply.github.com>
2026-03-04 21:55:23 -0500

    Time: 11 ms (24.81%) | Memory: 24.7 MB (26.05%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
-        freq = [[] for i in range(len(nums) + 1)] #empty array the size of our inputs + 1
+        freq = [[] for i in range(len(nums) + 1)]
 
         for n in nums:
-            count[n] = 1 + count.get(n, 0) #1 plus what its current count is
-
-        #going through each value that we counted
+            count[n] = 1 + count.get(n, 0)
+        
         for n, c in count.items():
-            freq[c].append(n) #value n appears c times #at index count we're going to append to the list this value n
-            #this value n occurs c numbers of time
-
+            freq[c].append(n)
+        
         res = []
-        for i in range(len(freq) - 1, 0, -1): #going descending order to check the most frequent k numbers -1 as the decrementer
-            for n in freq[i]:   
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
                     return res
