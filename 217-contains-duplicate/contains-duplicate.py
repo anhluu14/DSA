6dfd006c01a4a2f22be91c35acd4d85a3fc2e026
Anh Luu <101529755+anhluu14@users.noreply.github.com>
2026-03-05 23:53:49 -0500

    Time: 16 ms (33.15%) | Memory: 32.4 MB (26.80%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -5,5 +5,6 @@ class Solution:
         for n in nums:
             if n in hashset:
                 return True
-            hashset.add(n)
+            else:
+                hashset.add(n)
         return False
