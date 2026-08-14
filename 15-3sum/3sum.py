42bce7f0719f5036bb10b591472fec32ba12423a
Anh Luu <101529755+anhluu14@users.noreply.github.com>
2026-08-14 13:01:39 -0400

    Time: 495 ms (88.36%) | Memory: 22.5 MB (17.70%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -18,10 +18,13 @@ class Solution:
                 if sum == 0:
                     answer.append([nums[i], nums[lo], nums[hi]])
                     lo, hi = lo + 1, hi - 1
+
                     while lo < hi and nums[lo] == nums[lo-1]:
                         lo += 1
+
                     while lo < hi and nums[hi] == nums[hi +1]:
                         hi -= 1
+                        
                 elif sum < 0:
                     lo += 1
                 else:
